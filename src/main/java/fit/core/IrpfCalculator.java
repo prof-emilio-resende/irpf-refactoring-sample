e731fe157f652e74977334eff55ac44bf7338820
emilio.resende <emilio.resende@faculdadeimpacta.com.br>
2022-08-12 22:19:24 -0300

    + extract class

--- src/main/java/fit/core/IrpfCalculator.java
+++ src/main/java/fit/core/IrpfCalculator.java
@@ -1,55 +1,50 @@
 package fit.core;
 
+import fit.application.impl.DiscountTable;
+import fit.application.impl.RateTable;
 import fit.domain.Person;
 
 public class IrpfCalculator {
-  /**
-   *
-   */
-  private static final double EXEMPTION_VALUE_2021 = 1903.98;
-  private static final double INSS_VALUE_2021 = 0.11;
-  private static final double DEPENDENT_VALUE_2021 = 189.59;
-
   private int year;
   private Person person;
+  private RateTable rateTable;
+  private DiscountTable discountTable;
 
   public IrpfCalculator(int year, Person person) {
     super();
     this.year = year;
     this.person = person;
+    this.rateTable = new RateTable();
+    this.discountTable = new DiscountTable();
   }
 
   public double calculateInssValue() {
-    return this.person.getTotalSalary() * INSS_VALUE_2021;
+    return this.rateTable.calculateInssValue(this.person.getTotalSalary());
   }
 
   public double calculateDependentsValue() {
-    return this.person.getDependents() * DEPENDENT_VALUE_2021;
+    return this.discountTable.calculateDependentsValue(this.person.getDependents());
   }
 
   public double calculateBaseSalary() {
     return this.person.getTotalSalary() - this.calculateDependentsValue() - this.calculateInssValue();
   }
 
-  public static double calculateExemption() {
-    return EXEMPTION_VALUE_2021;
+  public double calculateExemption() {
+    return this.discountTable.calculateExemption();
   }
 
   public double calculateDiscount() {
-    return this.calculateInssValue() + IrpfCalculator.calculateExemption();
+    return this.calculateInssValue() + this.calculateExemption();
   }
 
-  public static double calculateTaxLayer(double baseSalary) {
-    if (baseSalary <= 1903.98) return 0.0;
-    if (baseSalary <= 2826.65) return 0.075;
-    if (baseSalary <= 3751.05) return 0.15;
-    if (baseSalary <= 4664.68) return 0.225;
-    return 0.275;
+  public double calculateTaxLayer(double baseSalary) {
+    return this.rateTable.calculateTaxLayer(baseSalary);
   }
 
   public double calculate() {
-    var salaryForIrpf = this.calculateBaseSalary() - IrpfCalculator.calculateExemption();
-    var taxValue = IrpfCalculator.calculateTaxLayer(this.calculateBaseSalary());
+    var salaryForIrpf = this.calculateBaseSalary() - this.calculateExemption();
+    var taxValue = this.calculateTaxLayer(this.calculateBaseSalary());
 
     return salaryForIrpf * taxValue;
   }
